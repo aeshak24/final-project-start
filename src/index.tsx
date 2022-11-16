a341e65688d554ce4fea7ed7a663d09e32823fbd
Danielle Livornese <daniellelivornese@wifi-roaming-128-4-126-70.wifi.udel.edu>
2022-11-16 15:01:42 -0500

    random

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM, { render } from "react-dom";
 import Board from "./Board";
 import "./index.css";
 import { observe } from "./game";
